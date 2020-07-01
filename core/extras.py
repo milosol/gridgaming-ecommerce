98b5cc21301e1ded57a1c5540d39c161bd8e2fdf
Pyweb-master <fam.mochih321@gmail.com>
2020-07-01 04:20:33 -0400

    removing braintree stuff

--- core/extras.py
+++ core/extras.py
@@ -1,20 +1,20 @@
 import braintree
-from django.conf import settings
+from decouple import config
 
-# gateway = braintree.BraintreeGateway(
-#     braintree.Configuration(
-#         environment=settings.BRAINTREE_ENVIRONMENT,
-#         merchant_id=settings.BRAINTREE_MERCHANT_ID,
-#         public_key=settings.BRAINTREE_PUBLIC_KEY,
-#         private_key=settings.BRAINTREE_PRIVATE_KEY
-#     )
-# )
-#
-# def generate_client_token():
-#     return gateway.client_token.generate()
-#
-# def transact(options):
-#     return gateway.transaction.sale(options)
-#
-# def find_transaction(id):
-#     return gateway.transaction.find(id)
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        environment=config("BRAINTREE_ENVIRONMENT"),
+        merchant_id=config("BRAINTREE_MERCHANT_ID"),
+        public_key=config("BRAINTREE_PUBLIC_KEY"),
+        private_key=config("BRAINTREE_PRIVATE_KEY")
+    )
+)
+
+def generate_client_token():
+    return gateway.client_token.generate()
+
+def transact(options):
+    return gateway.transaction.sale(options)
+
+def find_transaction(id):
+    return gateway.transaction.find(id)
