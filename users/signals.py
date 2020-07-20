3c9967cba11915e0339acac4405cde53a2cbf29e
Pyweb-master <fam.mochih321@gmail.com>
2020-07-20 12:58:12 -0400

    fix to users logging in and checking username

--- users/signals.py
+++ users/signals.py
@@ -1,20 +1,20 @@
-from allauth.socialaccount.signals import (
-    pre_social_login,
-    social_account_added,
-    social_account_removed,
-    social_account_updated
-)
-from allauth.account.signals import (
-    email_confirmed,
-    user_signed_up,
-    user_logged_in,
-    user_logged_out
-)
-
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from users.models import User
-from core.models import History
+# from allauth.socialaccount.signals import (
+#     pre_social_login,
+#     social_account_added,
+#     social_account_removed,
+#     social_account_updated
+# )
+# from allauth.account.signals import (
+#     email_confirmed,
+#     user_signed_up,
+#     user_logged_in,
+#     user_logged_out
+# )
+#
+# from django.dispatch import receiver
+# from django.contrib.auth.models import User
+# from users.models import User
+# from core.models import History
 
 
 # @receiver(user_signed_up)
@@ -30,18 +30,3 @@ from core.models import History
 #     user.profile.email_address = email
 #     #user.profile.first_name = first_name
 #     user.profile.save()
-
-@receiver(user_logged_in)
-def login_user(sociallogin, user, **kwargs):
-    History.objects.create(user=user, action='Logged In')
-    if sociallogin.account.provider == 'twitter':
-        print(sociallogin.account.extra_data)
-        name = sociallogin.account.extra_data['name']
-        user.first_name = name.split()[0]
-        user.last_name = name.split()[1]
-        user.save()
-
-@receiver(user_logged_out)
-def logged_out(sociallogin, user, **kwargs):
-    History.objects.create(user=user, action='Logged Out')
-        
