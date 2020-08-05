d31e74b5946d6483125a538703242e5996ef53e9
Pyweb-master <fam.mochih321@gmail.com>
2020-08-05 03:29:32 -0400

    adding gleam embed

--- giveaways/admin.py
+++ giveaways/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Giveaway
 
+
 # Register your models here.
 
 
 class GiveawayAdmin(admin.ModelAdmin):
-    list_display = ['title','description','url','image', 'giveaway_end_date']
+    list_display = ['title', 'description', 'url', 'gleam_embed', 'image', 'giveaway_end_date']
+
 
 admin.site.register(Giveaway, GiveawayAdmin)
-
