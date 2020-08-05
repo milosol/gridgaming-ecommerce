44210d087b336d8d2ea681a2c7727e69b4bdb2ff
Pyweb-master <fam.mochih321@gmail.com>
2020-08-05 04:47:53 -0400

    removing print statements

--- giveaways/models.py
+++ giveaways/models.py
@@ -20,6 +20,7 @@ class Giveaway(models.Model):
     giveaway_end_date = models.DateTimeField(null=True, blank=True)
     url = models.URLField(max_length=500, unique=True, blank=True, null=True)
     gleam_embed = models.TextField(null=True, blank=True)
+    gleam_graph_tags = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     slug = models.SlugField(
         default="", editable=False, max_length=settings.GIVEAWAY_TITLE_MAX_LENGTH
