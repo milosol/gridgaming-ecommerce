d1b6a97da18767fbf39cb88120fffcd4a07d50de
Pyweb-master <fam.mochih321@gmail.com>
2020-10-15 16:29:51 -0400

    pass if no account type

--- core/decorators.py
+++ core/decorators.py
@@ -10,6 +10,8 @@ def account_type_check(function):
                 messages.info(request, "Please select role before using the grid.")
                 return HttpResponseRedirect(reverse('frontend:account_type'))
             return function(request, *args, **kwargs)
+        else:
+            pass
         # else:
         #     return HttpResponseRedirect(HttpResponse('Error processing request. Please try again!'))
     return _function
