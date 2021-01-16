3e374457be5793d1954217e68785aa608ef91abe
Pyweb-master <fam.mochih321@gmail.com>
2021-01-16 08:13:52 -0500

    updated ads

--- frontend/ads.py
+++ frontend/ads.py
@@ -16,10 +16,21 @@ sovrn.com, 231887, DIRECT, fafdf38b16bf6b2b #sovrn
 contextweb.com, 560606, RESELLER, 89ff185a4c4e857c
 rhythmone.com, 1059622079, RESELLER, a670c89d4a324e47
 sonobi.com, 4ba74d4dae, DIRECT, d1a215d9eb5aee9e
+33across.com, 0013300001kQj2HAAS, RESELLER, bbea06d9c4d2853c
+indexexchange.com, 186046, RESELLER
+openx.com, 540274407, RESELLER, 6a698e2ec38604c6
+pubmatic.com, 156557, RESELLER
+pubmatic.com, 158723, RESELLER, 5d62403b186f2ace
+rubiconproject.com, 18694, RESELLER, 0bfd66d529a55807
+sharethrough.com, 2263985f, DIRECT, d53b998a7bd4ecd2
+sharethrough.com, 5629b363, DIRECT, d53b998a7bd4ecd2
+spotxchange.com, 212457, RESELLER
+spotx.tv, 212457, RESELLER
 pubmatic.com, 137711, RESELLER, 5d62403b186f2ace
 pubmatic.com, 156212, RESELLER, 5d62403b186f2ace
 pubmatic.com, 156972, DIRECT, 5d62403b186f2ace        #for PubMatic
 advertising.com, 24410, RESELLER
+advertising.com, 28738, RESELLER
 advertising.com, 7372, RESELLER
 freewheel.tv, 19129, RESELLER
 freewheel.tv, 19133, RESELLER
@@ -42,6 +53,7 @@ adtech.com, 11574, DIRECT
 adtech.com, 12094, RESELLER
 advangelists.com, 8d3bba7425e7c98c50f52ca1b52d3735, RESELLER, 60d26397ec060f98
 advertising.com, 28488, DIRECT
+advertising.com, 28738, RESELLER
 appnexus.com, 10239, RESELLER, f5ab79cb980f11d1
 appnexus.com, 10239, RESELLER, f5ab79cb980f11d1
 appnexus.com, 11429, DIRECT
@@ -51,6 +63,7 @@ appnexus.com, 3663, RESELLER, f5ab79cb980f11d1
 appnexus.com, 7597, RESELLER, f5ab79cb980f11d1
 appnexus.com, 7666, RESELLER, f5ab79cb980f11d1
 appnexus.com, 9316, RESELLER, f5ab79cb980f11d1
+chocolateplatform.com, 14546, DIRECT, 49a66ce31a704197
 coxmt.com, 2000067995202, RESELLER
 criteo.com, 7822, RESELLER
 districtm.io, 100962, RESELLER, 3fd707be9c4527c3
@@ -65,6 +78,7 @@ google.com, pub-9685734445476814, RESELLER, f08c47fec0942fa0
 gumgum.com, 11483, DIRECT, ffdef49475d318a9
 gumgum.com, 13318, RESELLER, ffdef49475d318a9
 gumgum.com, 13926, DIRECT, ffdef49475d318a9
+improvedigital.com, 1530, DIRECT
 indexexchange.com, 184533, DIRECT
 indexexchange.com, 185192, RESELLER
 indexexchange.com, 188838, DIRECT
@@ -73,9 +87,11 @@ lijit.com, 264726, DIRECT, fafdf38b16bf6b2b
 lijit.com, 264726-eb, DIRECT, fafdf38b16bf6b2b
 lijit.com, 273657, DIRECT, fafdf38b16bf6b2b
 lijit.com, 273657-eb, DIRECT, fafdf38b16bf6b2b
+media.net, 8CU713ZQC, DIRECT
 my6sense.com, 9732, RESELLER
 nobid.io, 21936924067, DIRECT
 openx.com, 537125314, RESELLER, 6a698e2ec38604c6
+openx.com, 537153564, RESELLER, 6a698e2ec38604c6
 openx.com, 538929384, RESELLER, 6a698e2ec38604c6
 openx.com, 539392223, RESELLER, 6a698e2ec38604c6
 openx.com, 539392223, RESELLER, 6a698e2ec38604c6
@@ -96,6 +112,7 @@ rubiconproject.com, 156042, RESELLER, 0bfd66d529a55807
 rubiconproject.com, 16414, RESELLER, 0bfd66d529a55807
 rubiconproject.com, 16414, RESELLER, 0bfd66d529a55807
 rubiconproject.com, 17346, RESELLER, 0bfd66d529a55807
+sonobi.com, f22036b5f1, DIRECT, d1a215d9eb5aee9e
 sovrn.com, 264726, DIRECT, fafdf38b16bf6b2b
 sovrn.com, 273657, DIRECT, fafdf38b16bf6b2b
 spotxchange.com, 108706, RESELLER, 7842df1d2fe2db34
@@ -103,11 +120,16 @@ spotxchange.com, 82069, DIRECT, 7842df1d2fe2db34
 spotx.tv, 108706, RESELLER, 7842df1d2fe2db34
 spotx.tv, 82069, DIRECT, 7842df1d2fe2db34
 synacor.com, 82151, RESELLER, e108f11b2cdf7d5b
+telaria.com, 32jid-ohl8i, DIRECT, 1a4e959a1b50034a
 telaria.com, jrkl5-0ac26, RESELLER, 1a4e959a1b50034a
+tremorhub.com, 32jid-ohl8i, DIRECT, 1a4e959a1b50034a
 tremorhub.com, jrkl5-0ac26, RESELLER, 1a4e959a1b50034a
 triplelift.com, 6030-EB, DIRECT, 6c33edb13117fd86
 undertone.com, 3231, DIRECT
 undertone.com, 3802, DIRECT
+vdopia.com, 14546, DIRECT, 49a66ce31a704197
+video.unrulymedia.com, 203808374, DIRECT
+yieldmo.com, 2230484298816456318, DIRECT
 beachfront.com, 5880, DIRECT, e2541279e8e2ca4d
 beachfront.com, publisher_5880, DIRECT          #for BEACHFRONT
 appnexus.com, 2854, RESELLER                     #for APPNEXUS
@@ -124,29 +146,29 @@ indexexchange.com, 187819, DIRECT, 50b1c356f2c5c8fc  #for Index EB
 pubmatic.com, 157200, DIRECT, 5d62403b186f2ace #for Pubmatic EB
 rhythmone.com, 32202710, DIRECT, a670c89d4a324e47 #for RhythmOne EB
 33across.com, 0013300001ehumFAAQ, DIRECT, bbea06d9c4d2853c #33Across #hb #tag
-adtech.com, 12094, RESELLER #33Across #hb #tag
-adtech.com, 9993, RESELLER #33Across #tag
-advangelists.com, 8d3bba7425e7c98c50f52ca1b52d3735, RESELLER, 60d26397ec060f98 #33Across #hb #tag
-aol.com, 47594, RESELLER, e1a5b5b6e3255540 #33Across #hb #tag #viewable
+adtech.com, 12068, RESELLER, e1a5b5b6e3255540 #33Across #tag
 appnexus.com, 1001, RESELLER, f5ab79cb980f11d1 #33Across #tag
 appnexus.com, 10239, RESELLER, f5ab79cb980f11d1 #33Across #hb #tag #viewable
+appnexus.com, 1908, RESELLER, f5ab79cb980f11d1 #33Across #tag
 appnexus.com, 3135, RESELLER, f5ab79cb980f11d1 #33Across #tag
+appnexus.com, 3663, RESELLER, f5ab79cb980f11d1 #33Across #tag
+aps.amazon.com, 2840f06c-5d89-4853-a03e-3bfa567dd33c, DIRECT #33Across #tag
 google.com, pub-9557089510405422, RESELLER, f08c47fec0942fa0 #33Across #tag
-indexexchange.com, 183635, RESELLER, 50b1c356f2c5c8fc #33Across #hb #tag #viewable
 indexexchange.com, 190966, RESELLER, 50b1c356f2c5c8fc #33Across #tag #ebda
 indexexchange.com, 191973, RESELLER, 50b1c356f2c5c8fc #33Across #hb #tag #viewable
 openx.com, 537120563, RESELLER, 6a698e2ec38604c6 #33Across #hb #tag
 openx.com, 539392223, RESELLER, 6a698e2ec38604c6 #33Across #tag #ebda
-openx.com, 540995201, RESELLER, 6a698e2ec38604c6 #33Across #hb #tag #viewable
+openx.com, 540191398, RESELLER, 6a698e2ec38604c6 #33Across #tag
 pubmatic.com, 156423, RESELLER, 5d62403b186f2ace #33Across #hb #tag
-pubmatic.com, 158136, RESELLER, 5d62403b186f2ace #33Across EU #hb #tag
+pubmatic.com, 157150, RESELLER, 5d62403b186f2ace #33Across #tag
 pubmatic.com, 158569, RESELLER, 5d62403b186f2ace #33Across #tag #ebda
 rubiconproject.com, 16414, RESELLER, 0bfd66d529a55807 #33Across #hb #tag
+rubiconproject.com, 18020, RESELLER, 0bfd66d529a55807 #33Across #tag
 rubiconproject.com, 21434, RESELLER, 0bfd66d529a55807 #33Across #tag #ebda
 rubiconproject.com, 21642, RESELLER, 0bfd66d529a55807 #33Across #hb #tag #viewable
-rubiconproject.com, 21720, RESELLER, 0bfd66d529a55807 #33Across EU #hb #tag
 sonobi.com, a416546bb7, RESELLER, d1a215d9eb5aee9e  #33Across #tag #ebda
-yahoo.com, 57289, RESELLER,  e1a5b5b6e3255540 #33Across #hb #tag
+yahoo.com, 55029, RESELLER, e1a5b5b6e3255540 #33Across #tag
+yahoo.com, 57289, RESELLER, e1a5b5b6e3255540
 """
 
 
